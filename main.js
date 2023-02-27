080a75aef220e3fda72e2fdc4fef74efbb832cd2
wint3rly7 <126253323+wint3rly7@users.noreply.github.com>
2023-02-27 19:30:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// pentru ecran loader de la inceputul sitului
+window.addEventListener('load',function(){
+    document.querySelector('body').classList.add("loaded")  
+  });
+
+function version2() {
+  alert("Vrei intradevar sa trimiti datele tale?")
+}
+
